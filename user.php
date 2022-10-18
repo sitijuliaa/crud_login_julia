034bf42141ef7a51714f6cddac7fc88e93e188f2
sitijuliaa <110010453+sitijuliaa@users.noreply.github.com>
2022-10-18 10:58:51 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="style.css" rel="styleesheet">
+  </head>
+  <body>
+    <body style="background-color:gray:">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 offset-md-3">
+          <marquee behavior="" direcction=""><h1>PEDULI DIRI></h></marquee>
+              <div class="card my-5">
+                <form class="card-body cardbody-color p-lg-5">
+                <div class="text-center">
+
+    <h1>catatan perjalanan</h1>
+    <a href="user.php">HOMEI </a>
+    <a href="catatan.php">catatan perjalanan </a>
+    <a href="isi_data.php">isi_data.php </a>
+       </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
